9515e994e8fe0f619f961d72cb0dc60f28b50c62
Isabel <maria.perez1.mt@usa.edu.co>
2022-09-21 17:24:59 -0500

    [update] Added clients message html files

--- app.js
+++ app.js
@@ -1,1 +1,114 @@
-console.log(123)
+console.log(4+4)
+
+
+function sumTwoNumbers(){
+    return 4+5
+}
+
+function changeContent(){
+    document.getElementById("toChange").innerHTML="cambio desde javascript"
+
+}
+
+function getCostumes(){
+    const $responseContainer = document.getElementById("response");
+    //$responseContainer.innerHTML='Texto agregado desde javascript';
+    $.ajax({
+        url:"https://g733f2e5f01a568-i3uzynq7iaxfveju.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costumes",
+        type: "GET",
+        datatype:"",
+        success:function(response){
+            console.log(response.items)
+            for(let x=0; x<response.items.length;x++){
+                const costume = response.items[x];
+                $responseContainer.innerHTML+=`
+                id:${costume.idcostume}
+                nombre:${costume.name}
+                marca:${costume.brand}
+                años:${costume.year}
+                descripcion:${costume.description}
+                categoria:${costume.idcategory}`+"<br>";
+            }
+
+        }
+        
+
+    })
+
+}
+
+function sendCostume(){
+    let dataToSend={
+        "idcostume":parseInt($("#idcostume").val()),
+        "name":$("#name").val(),
+        "brand":$("#brand").val(),
+        "year":parseInt($("#year").val()),
+        "description":$("#description").val(),
+        "idcategory":parseInt($("#idcategory").val()) 
+    }
+    dataToSend=JSON.stringify(dataToSend);
+
+    const settings ={
+        url:"https://g733f2e5f01a568-i3uzynq7iaxfveju.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costumes",
+        type: "POST",
+        "headers" : {
+            "Content-Type":"application/json"
+        },
+        "data": dataToSend,
+    }; 
+
+    $.ajax(settings).done(function (response){
+            console.log(response)
+    });
+}
+
+function updateCostume(){
+    let dataToSend={
+        "idcostume":parseInt($("#idcostume").val()),
+        "name":$("#name").val(),
+        "brand":$("#brand").val(),
+        "year":parseInt($("#year").val()),
+        "description":$("#description").val(),
+        "idcategory":parseInt($("#idcategory").val()) 
+    }
+    dataToSend=JSON.stringify(dataToSend);
+
+    const settings ={
+        url:"https://g733f2e5f01a568-i3uzynq7iaxfveju.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costumes",
+        type: "PUT",
+        "headers" : {
+            "Content-Type":"application/json"
+        },
+        "data": dataToSend,
+    }; 
+    
+    $.ajax(settings).done(function (response){
+            console.log(response)
+    });
+
+}
+
+function deleteCostume(){
+    let dataToSend={
+        "idcostume":parseInt($("#idcostume").val()),
+    }
+    dataToSend=JSON.stringify(dataToSend);
+
+    const settings ={
+        url:"https://g733f2e5f01a568-i3uzynq7iaxfveju.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/api/costumes",
+        type: "DELETE",
+        "headers" : {
+            "Content-Type":"application/json"
+        },
+        "data": dataToSend,
+    }; 
+    
+    $.ajax(settings).done(function (response){
+            console.log(response)
+    });
+
+}
+
+getCostumes()
+console.log(sumTwoNumbers())
+changeContent()
